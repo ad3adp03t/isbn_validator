54caa9d2ca1ca0cbfe158901a0ea137928b938a6
ad3adp03t <ad3adp03t@gmail.com>
2017-08-30 15:07:37 -0400

    started trying to code checker of alpha characters

--- isbnapp.rb
+++ isbnapp.rb
@@ -1,13 +1,42 @@
-def isbn_cleaner(isbn)
-	isbn = isbn.gsub(/[-]/, '')
-	isbn = isbn.gsub(/[ ]/, '')
-end	
-def check_digit_generator(isbn)
-	arr=[]
-	isbn = isbn.chars
-	isbn.each_with_index do |va,ind|
-		arr.push(va.to_i * (ind+1).to_i)
+# def isbn_cleaner(isbn)
+# 	isbn = isbn.gsub(/[-]/, '')
+# 	isbn = isbn.gsub(/[ ]/, '')
+# end	
+def isbn_alpha_check(isbn)
+	isbn.to_i.chars
+	isbn.each_with_index do |num,ind|
+		while ind<=8
+			if num is == *(A..Z) || *(a..z)
+				p "Invalid Number"
+		end	
+		while ind==9
+			if num is == *(A..W)|| *(a..w)||Y||y||Z||z
+				p "Invaid Number"
+		end
 	end
-	p arr	
-end	
-p 	check_digit_generator("407234058")
+end				
+
+
+p isbn_alpha_check("8637a028b0")
+p isbn_alpha_check("401703793x")
+
+# def isbn_length_check(isbn)
+# 	isbn = isbn_cleaner(isbn)
+# 	if isbn.length == 10
+# 		true
+# 	else
+# 		false
+# 	end
+# end		
+# def check_digit_generator(isbn)
+# 	arr=[]
+# 	isbn = isbn.chars
+# 	isbn.each_with_index do |va,ind|
+# 		arr.push(va.to_i * (ind+1).to_i)
+# 	end
+# 	sum = 0
+# 	arr.each { |a| sum+=a }
+# 	sum = sum%11
+# end	
+	
+# p 	check_digit_generator("047195869")
