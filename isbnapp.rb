089e4ab9c662f5b801b830892d3e490eff539c1b
ad3adp03t <ad3adp03t@gmail.com>
2017-08-30 12:20:42 -0400

    added program to multiply based on position

--- isbnapp.rb
+++ isbnapp.rb
@@ -1,11 +1,13 @@
-def dash_remove(isbn)
-	isbn.gsub!(/[-]/, '')
+def isbn_cleaner(isbn)
+	isbn = isbn.gsub(/[-]/, '')
+	isbn = isbn.gsub(/[ ]/, '')
 end	
-
-p dash_remove("12-345-67") 
-
-def space_remove(isbn)
-	isbn.gsub!(/[ ]/, '')
+def check_digit_generator(isbn)
+	arr=[]
+	isbn = isbn.chars
+	isbn.each_with_index do |va,ind|
+		arr.push(va.to_i * (ind+1).to_i)
+	end
+	p arr	
 end	
-
-p space_remove("12 345 67") 
+p 	check_digit_generator("407234058")
--- test.rb
+++ test.rb
@@ -9,5 +9,9 @@ class TestISBN < Minitest::Test
   		num ="6836401867"
   		assert_equal(num.length,10)
   	end	
+  	def test_if_arr
+  		num ="123456"
+  		assert_equal(["1","2","3","4","5","6"],check_digit_generator(num))
+  	end	
 
 end
