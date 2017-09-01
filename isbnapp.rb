dd13ecc8d77b83168c288eb128226f8361b7b88b
ad3adp03t <ad3adp03t@gmail.com>
2017-09-01 10:54:20 -0400

    added tho check digit generator to compare to last digit

--- isbnapp.rb
+++ isbnapp.rb
@@ -1,24 +1,24 @@
 # def isbn_cleaner(isbn)
-# 	isbn = isbn.gsub(/[-]/, '')
-# 	isbn = isbn.gsub(/[ ]/, '')
+# 	isbn = isbn.gsub(/[-]/,'')
+# 	isbn = isbn.gsub(/[ ]/,'')
 # end	
-def isbn_alpha_check(isbn)
-	isbn.to_i.chars
-	isbn.each_with_index do |num,ind|
-		while ind<=8
-			if num is == *(A..Z) || *(a..z)
-				p "Invalid Number"
-		end	
-		while ind==9
-			if num is == *(A..W)|| *(a..w)||Y||y||Z||z
-				p "Invaid Number"
-		end
-	end
-end				
+# def isbn_alpha_check(isbn)
+# 	isbn = isbn.chars
+# 	last = isbn.pop
+# 	if last == "x"||"X"
+# 		isbn.push("10")
+# 	else
+# 		isbn.push(last)	
+# 	end
+# 		if isbn.include?('a'||'b'||'c'||'d'||'e'||'f'||'g'||'h'||'i'||'j'||'k'||'l'||'m'||'n'||'o'||'p'||'q'||'r' ||'s'||'t'||'u'||'v'||'w'||'x'||'y'||'z'||'A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'Q'||'R'||'S'||'T'||'U'||'V'||'W'||'X'||'Y'||'Z')
+# 	# if isbn.include?("A".."Z")||("a".."z")
+# 		p "Not A Valid ISBN"
+# 	end	
+# end				
 
 
-p isbn_alpha_check("8637a028b0")
-p isbn_alpha_check("401703793x")
+# #p isbn_alpha_check("8637a028b0")
+# p isbn_alpha_check("401703793x")
 
 # def isbn_length_check(isbn)
 # 	isbn = isbn_cleaner(isbn)
@@ -28,15 +28,25 @@ p isbn_alpha_check("401703793x")
 # 		false
 # 	end
 # end		
-# def check_digit_generator(isbn)
-# 	arr=[]
-# 	isbn = isbn.chars
-# 	isbn.each_with_index do |va,ind|
-# 		arr.push(va.to_i * (ind+1).to_i)
-# 	end
-# 	sum = 0
-# 	arr.each { |a| sum+=a }
-# 	sum = sum%11
-# end	
+def check_digit_generator(isbn)
+	arr=[]
+	isbn = isbn.chars
+	check = isbn.pop
+	isbn.each_with_index do |va,ind|
+		arr.push(va.to_i * (ind+1).to_i)
+	end
+	sum = 0
+	arr.each { |a| sum+=a }
+	sum = sum%11
+	check = check.to_i
+	if sum == check 
+		p "This Is A valid ISBN"
+	else 
+		p "This Is Not A Valid ISBN"
+	end		
+end	
+
+
+
 	
-# p 	check_digit_generator("047195869")
+ p 	check_digit_generator("0471958697")
