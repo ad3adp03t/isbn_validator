3726726e387df914e667101d966a0ae31ad65eb3
ad3adp03t <ad3adp03t@gmail.com>
2017-09-11 19:47:27 -0400

    Finally fixed error with check  digit generator for isbn10

--- isbnapp.rb
+++ isbnapp.rb
@@ -2,20 +2,7 @@
 # 	isbn = isbn.gsub(/[-]/,'')
 # 	isbn = isbn.gsub(/[ ]/,'')
 # end	
-# def isbn_alpha_check(isbn)
-# 	isbn = isbn.chars
-# 	last = isbn.pop
-# 	if last == "x"||"X"
-# 		isbn.push("10")
-# 	else
-# 		isbn.push(last)	
-# 	end
-# 		if isbn.include?('a'||'b'||'c'||'d'||'e'||'f'||'g'||'h'||'i'||'j'||'k'||'l'||'m'||'n'||'o'||'p'||'q'||'r' ||'s'||'t'||'u'||'v'||'w'||'x'||'y'||'z'||'A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'Q'||'R'||'S'||'T'||'U'||'V'||'W'||'X'||'Y'||'Z')
-# 	# if isbn.include?("A".."Z")||("a".."z")
-# 		p "Not A Valid ISBN"
-# 	end	
-# end				
-
+			
 
 # #p isbn_alpha_check("8637a028b0")
 # p isbn_alpha_check("401703793x")
@@ -28,25 +15,116 @@
 # 		false
 # 	end
 # end		
-def check_digit_generator(isbn)
+
+
+# def alpha_check10(isbn)
+# 	last = last
+# 	upper = *("A".."Z")
+# 	lower = *("a".."z")
+# 	isbn = isbn.chars
+# 	last = isbn.pop
+# 	if last == "x"
+# 		isbn.push("10")
+# 	else
+# 		isbn.push(last)
+# 	end	
+# 	p isbn
+# 	if isbn.include?(upper) 
+# 		p "Not a valid ISBN"
+# 	elsif isbn.include?(lower)
+# 		p "not a valid ISBN"
+# 	else	
+# 		p "Valid"
+# 	end
+		
+# end		
+
+
+
+# def alpha_check10(isbn)
+# 	last = last
+# 	upper = *("A".."Z")
+# 	lower = *("a".."z")
+# 	isbn = isbn.chars
+# 	last = isbn.pop
+# 	if last == "x"
+# 		isbn.push("10")
+# 	else
+# 		isbn.push(last)
+# 	end	
+	 
+# 	if (upper-isbn).size == 26&& (lower-isbn).size == 26
+# 		p "Valid ISBN"
+# 	else	
+# 		p "not valid"
+# 	end
+# end	
+# alpha_check10("12355679")
+
+# def alpha_check13(isbn)
+# 	upper = *("A".."Z")
+# 	lower = *("a".."z")
+# 	isbn = isbn.chars
+	 
+# 	if (upper-isbn).size == 26&& (lower-isbn).size == 26
+# 		p "Valid ISBN"
+# 	else	
+# 		p "not valid"
+# 	end
+# end	
+# alpha_check13("123556798S387")
+
+def check_digit_generator10(isbn)
 	arr=[]
 	isbn = isbn.chars
 	check = isbn.pop
+	check = check.to_i
+	
 	isbn.each_with_index do |va,ind|
 		arr.push(va.to_i * (ind+1).to_i)
-	end
+	end	
+	
 	sum = 0
 	arr.each { |a| sum+=a }
 	sum = sum%11
-	check = check.to_i
-	if sum == check 
+	if check == "x"||"X" && sum == 10
+		p "Valid"
+	elsif sum == check 
 		p "This Is A valid ISBN"
 	else 
 		p "This Is Not A Valid ISBN"
 	end		
 end	
+ p 	check_digit_generator10("080442957X")
+
+# def check_digit_13(isbn)
+# 	arr = []
+# 	sum = 0
+# 	isbn = isbn.chars
+# 	check = isbn.pop.to_i
+# 	isbn.each_with_index do |x,y|
+# 		if (y+1) % 2 == 0
+# 			arr.push(x.to_i*3)
+# 		else
+# 			arr.push(x.to_i)
+# 		end
+# 	end		
+# 	arr.each {|a|sum+=a}
+# 	sum = sum%10 
+# 	sum = (10-sum)
+# 	sum = sum%10
+# 	p check
+# 	p sum
+# 	if check == sum
+# 		p "valid check digit"
+# 	else 
+# 		p "not valid"	
+# 	end	
+# end
+# p check_digit_13("9780470059029")
+
+
+				
 
 
 
-	
- p 	check_digit_generator("0471958697")
